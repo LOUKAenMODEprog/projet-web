9aca6c3bb13e845d1f2421d2fc817abbe1258bb9
LOUKAenMODEprog <novakovicnadeaul@seminaire-sherbrooke.qc.ca>
2026-02-06 23:52:48 +0000

    patate

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+document.getElementById("button_killian").onmousedown = Killian;
+document.getElementById("button_killian").onmouseup = killian1;
+document.getElementById("button_test").onclick = test;
+document.getElementById("change_couleur").onclick = couleur;
 
-document.getElementById("button_killian").onclick = Killian();
+
 
 function Killian() {
     document.getElementById("killian").style.display = "block";
+}
+
+function killian1() {
+    document.getElementById("killian").style.display = "none";
+}
+
+function test() {
+    document.getElementById("test_id").style.display = "block";
+}
+
+function couleur() {
+    document.body.style.backgroundColor = "#8d02ff";
 }
